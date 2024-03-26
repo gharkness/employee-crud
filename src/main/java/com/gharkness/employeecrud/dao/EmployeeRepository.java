4abe404ff3419a6231678cfe63901cd1b84d478c
Garrett Harkness <gharkness@sourceallies.com>
2024-03-26 08:26:40 -0500

    Converted to use spring-data-rest to remove service and rest controllers

--- src/main/java/com/gharkness/employeecrud/dao/EmployeeRepository.java
+++ src/main/java/com/gharkness/employeecrud/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.gharkness.employeecrud.dao;
 
 import com.gharkness.employeecrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
